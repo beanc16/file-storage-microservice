fdcb39a52a3e71ab641330ac1b71e72f8a939a37
beanc16 <bean.christopher@outlook.com>
2022-07-18 15:06:26 -0400

    Allow forward slashes in nested folders

--- src/api/v1/public/validation/schemas/files/helpers/nestedFolders.js
+++ src/api/v1/public/validation/schemas/files/helpers/nestedFolders.js
@@ -2,8 +2,8 @@ const Joi = require("joi");
 
 
 
-// Lowercase and hyphen
-const _nestedFoldersRegex = /^[a-z\-]+$/;
+// Lowercase, hyphen, and forward slash
+const _nestedFoldersRegex = /^[a-z\-/]+$/;
 
 const nestedFolders = Joi.string().pattern(_nestedFoldersRegex);
 const nestedFoldersRequired = nestedFolders.required();
