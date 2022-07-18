820248bb7449b09f7ad360e4453421392b7c3147
beanc16 <bean.christopher@outlook.com>
2022-07-18 14:58:41 -0400

    Update app schema

--- src/api/v1/public/validation/schemas/files/helpers/app.js
+++ src/api/v1/public/validation/schemas/files/helpers/app.js
@@ -1,11 +1,18 @@
 const Joi = require("joi");
+const { JoiRequired } = require("@beanc16/joi-helpers");
 
 
 
-const app = Joi.object({
-    id: Joi.string(),
-    searchName: Joi.string(),
-});
+const app = Joi.alternatives().try(
+    JoiRequired.object({
+        id: JoiRequired.string(),
+        searchName: Joi.string(),
+    }),
+    JoiRequired.object({
+        id: Joi.string(),
+        searchName: JoiRequired.string(),
+    }),
+);
 const appRequired = app.required();
 
 
