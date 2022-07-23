81e87464be535bc7700f8d03cf1948df01c5794d
beanc16 <bean.christopher@outlook.com>
2022-07-23 19:46:02 -0400

    Update upload payload validation

--- src/api/v1/public/validation/schemas/files/upload.js
+++ src/api/v1/public/validation/schemas/files/upload.js
@@ -6,7 +6,7 @@ const fileSchemas = require("./helpers");
 
 const uploadFilesSchema = JoiRequired.object({
     app: fileSchemas.appRequired,
-    fileName: fileSchemas.fileNameRequired,
+    file: fileSchemas.fileRequired,
     nestedFolders: fileSchemas.nestedFolders,
 });
 
