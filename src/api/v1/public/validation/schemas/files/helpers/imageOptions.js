0f1b732617daa524c9b4b6bb167fd0ee3b8e8f50
beanc16 <bean.christopher@outlook.com>
2024-08-28 20:57:03 -0400

    Add background recoloring

--- src/api/v1/public/validation/schemas/files/helpers/imageOptions.js
+++ src/api/v1/public/validation/schemas/files/helpers/imageOptions.js
@@ -2,7 +2,35 @@ const Joi = require("joi");
 
 
 
+const cloudinaryBackground = Joi.alternatives().try(
+    Joi.string().valid(
+        'white',
+        'grey',
+        'gray',
+        'lightgrey',
+        'lightgray',
+        'black',
+        'pink',
+        'lightpink',
+        'magenta',
+        'red',
+        'orange',
+        'yellow',
+        'lightyellow',
+        'green',
+        'lightgreen',
+        'blue',
+        'lightblue',
+        'purple',
+        'chocolate',
+        'transparent',
+        'none'
+    ),
+    Joi.string().regex(/^#[A-Fa-f0-9]{6}$/)
+);
+
 const imageOptions = Joi.object({
+    background: cloudinaryBackground,
     effect: Joi.string().valid('upscale'),
 });
 const imageOptionsRequired = imageOptions.required();
