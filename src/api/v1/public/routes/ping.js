25905024c3a1bff34fc1d29d01d213c846fe0df3
beanc16 <bean.christopher@outlook.com>
2025-02-10 12:35:05 -0500

    Remove unnecessary ping routes

--- src/api/v1/public/routes/ping.js
+++ src/api/v1/public/routes/ping.js
@@ -1,68 +1,9 @@
-/** **********
- * REQUIRES *
- *********** */
-
-// Routing
+const { Success } = require('dotnet-responses');
 const express = require('express');
 
 const app = express();
 
-// Response
-const { Success } = require('dotnet-responses');
-
-/** ******
- * GETS *
- ******* */
-
 app.get('/', (_req, res) =>
-{
-    Success.json({
-        res,
-        message: 'Pong',
-    });
-});
-
-/** *******
- * POSTS *
- ******** */
-
-app.post('/', (_req, res) =>
-{
-    Success.json({
-        res,
-        message: 'Pong',
-    });
-});
-
-/** *****
- * PUT *
- ****** */
-
-app.put('/', (_req, res) =>
-{
-    Success.json({
-        res,
-        message: 'Pong',
-    });
-});
-
-/** *********
- * PATCHES *
- ********** */
-
-app.patch('/', (_req, res) =>
-{
-    Success.json({
-        res,
-        message: 'Pong',
-    });
-});
-
-/** *********
- * DELETES *
- ********** */
-
-app.delete('/', (_req, res) =>
 {
     Success.json({
         res,
